7dfd57ded575cc76fe87cb8bcbc9b78a3acb5ed1
Phillip Cloud <cpcloud@gmail.com>
2014-05-28 13:47:26 -0400

    ENH: let bq remove nulls

--- nb/query.sql
+++ nb/query.sql
@@ -1,3 +1,5 @@
 SELECT payload_pull_request_additions, payload_pull_request_created_at
-       FROM [publicdata:samples.github_timeline] 
-       WHERE repository_name = 'pandas'
+           FROM [publicdata:samples.github_timeline]
+           WHERE repository_name = 'pandas' and
+           payload_pull_request_additions is not null and
+           payload_pull_request_created_at is not null
